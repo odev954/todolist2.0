9c8d00ef06072c40bcb1b0a75afc38d2c34e1529
odev954 <kleinbottle175@gmail.com>
2022-09-21 10:20:46 +0300

    setup to index

---
+++ src/index.js
@@ -0,0 +1,25 @@
+function main()
+{
+    let container = document.getElementById('test');
+    let menu = document.getElementById('menu');
+    let test1 = Task('testing this shit', 'test1', new Date(Date.now()));
+    let test2 = Task('testing this shit', 'test2', new Date(Date.now()));
+    let test3 = Task('testing this shit', 'test3', new Date(Date.now()));
+    let list = TaskList();
+    let ins = InsertButton(container, list);
+    let rmv = RemoveButton(container, list);
+    container.append(list.render(['cards-grid']));
+    
+    list.add(test1);
+    list.add(test2);
+    let element = list.add(test3);
+    
+   
+    container.append(element);
+    menu.append(ins.render([]));
+    menu.append(rmv.render([]));
+    
+    ins.components.button.addEventListener('click', () => rmv.setupClickEvents());
+}
+
+main()
